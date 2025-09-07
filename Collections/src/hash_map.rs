0c020a507a6bce4d2d00283ee314f9f74fe21d07
Andrei.Tokmakov <andtokm@yandex.ru>
2025-09-07 20:00:29 +0400

    07_09_2025_005

--- Collections/src/hash_map.rs
+++ Collections/src/hash_map.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub fn create()
 {
-    let mut scores = HashMap::new();
+    let mut scores: HashMap<String, i32> = HashMap::new();
 
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
@@ -38,6 +38,7 @@ fn iterate()
 fn add_or_insert()
 {
     let mut scores: HashMap<String, i32> = HashMap::new();
+
     scores.insert(String::from("Blue"), 10);
 
     println!("{:?}", scores);
@@ -56,7 +57,7 @@ fn update_values()
     scores.entry(String::from("Yellow")).or_insert(20);
     scores.entry(String::from("Blue")).insert_entry(30);
 
-    println!("{:?}", scores);
+    println!("{:?}", &scores);
 }
 
 fn count_words_in_the_text()
@@ -72,6 +73,70 @@ fn count_words_in_the_text()
     println!("{:?}", counter);
 }
 
+fn check_contains_key()
+{
+    let mut table: HashMap<String, i32> = HashMap::new();
+
+    table.insert(String::from("One"), 1);
+    table.insert(String::from("Two"), 2);
+    table.insert(String::from("Three"), 3);
+
+    for (key, value) in &table {
+        println!("{}: {}", key, value);
+    }
+
+    let key: String = String::from("One");
+    if table.contains_key("One") {
+        println!("{} exists", &key);
+    }
+}
+
+fn check_contains_key_list()
+{
+    let mut table: HashMap<String, i32> = HashMap::new();
+
+    table.insert(String::from("One"), 1);
+    table.insert(String::from("Two"), 2);
+    table.insert(String::from("Three"), 3);
+
+    // Look up the values associated with some keys.
+    let keys_to_find = ["One", "Two", "Five"];
+    for &key in &keys_to_find {
+        match table.get(key) {
+            Some(value) => println!("{key} ==> {value}"),
+            None => println!("{key} not found.")
+        }
+    }
+}
+
+fn delete_key()
+{
+    let mut table: HashMap<String, i32> = HashMap::new();
+
+    table.insert(String::from("One"), 1);
+    table.insert(String::from("Two"), 2);
+    table.insert(String::from("Three"), 3);
+
+    for (key, value) in &table {
+        println!("{}: {}", key, value);
+    }
+
+    let key: String = String::from("One");
+    if table.contains_key("One") {
+        println!("{} exists", &key);
+    }
+
+    table.remove(&key);
+
+
+    if table.contains_key("One") {
+        println!("{} exists", &key);
+    }
+    else {
+        println!("{} not found", &key);
+    }
+}
+
 pub fn test_all()
 {
     // create();
@@ -79,5 +144,10 @@ pub fn test_all()
     // iterate();
     // add_or_insert();
     // update_values();
-    count_words_in_the_text();
+    // count_words_in_the_text();
+
+    // check_contains_key();
+    check_contains_key_list();
+
+    // delete_key();
 }
