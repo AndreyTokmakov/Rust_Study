0a2413f0ef19d0c57d303ff57b5f1cf0774cc8b6
Andrei.Tokmakov <andtokm@yandex.ru>
2025-09-24 22:21:17 +0400

    24_09_2025_006

--- Cryptogrphy_Encryption/src/openssl.rs
+++ Cryptogrphy_Encryption/src/openssl.rs
@@ -5,27 +5,27 @@ mod signing
 {
     use openssl::sign::{Signer, Verifier};
     use openssl::rsa::Rsa;
-    use openssl::pkey::PKey;
+    use openssl::pkey::{PKey, Private};
     use openssl::hash::MessageDigest;
 
     // https://docs.rs/openssl/latest/openssl/sign/index.html
     pub fn sign_and_verify ()
     {
         // Generate a keypair
-        let keypair = Rsa::generate(2048).unwrap();
-        let keypair = PKey::from_rsa(keypair).unwrap();
+        let keypair: Rsa<Private> = Rsa::generate(2048).unwrap();
+        let keypair: PKey<Private> = PKey::from_rsa(keypair).unwrap();
 
         let data = b"hello, world!";
         let data2 = b"hola, mundo!";
 
         // Sign the data
-        let mut signer = Signer::new(MessageDigest::sha256(), &keypair).unwrap();
+        let mut signer: Signer = Signer::new(MessageDigest::sha256(), &keypair).unwrap();
         signer.update(data).unwrap();
         signer.update(data2).unwrap();
-        let signature = signer.sign_to_vec().unwrap();
+        let signature: Vec<u8> = signer.sign_to_vec().unwrap();
 
         // Verify the data
-        let mut verifier = Verifier::new(MessageDigest::sha256(), &keypair).unwrap();
+        let mut verifier: Verifier = Verifier::new(MessageDigest::sha256(), &keypair).unwrap();
         verifier.update(data).unwrap();
         verifier.update(data2).unwrap();
         assert!(verifier.verify(&signature).unwrap());
