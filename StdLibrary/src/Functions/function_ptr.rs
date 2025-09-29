f45e06efd0f1f8b8ca710c1035f742b4d4264d1a
Andrei.Tokmakov <andtokm@yandex.ru>
2025-09-29 20:48:26 +0400

    29_09_2025_08

--- StdLibrary/src/Functions/function_ptr.rs
+++ StdLibrary/src/Functions/function_ptr.rs
@@ -1,12 +1,44 @@
 
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+mod example_1
+{
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
+    pub fn demo()
+    {
+        let f: fn(i32, i32) -> i32 = add;
+        println!("2 + 3 = {}", f(2, 3));
+    }
 }
+
+mod example_2
+{
+    type FnPtr = fn() -> String;
+
+    fn add_field() -> String {
+        "add field".to_string()
+    }
+
+    fn remove_field() -> String {
+        "remove field".to_string()
+    }
+
+    pub fn demo()
+    {
+        let ptrFun: FnPtr = add_field;
+        println!("ptrFun: {}", ptrFun());
+
+        let ptrFun: FnPtr = remove_field;
+        println!("ptrFun: {}", ptrFun());
+    }
+}
+
 
 
 pub fn test_all()
 {
-    let f: fn(i32, i32) -> i32 = add;
-    println!("2 + 3 = {}", f(2, 3));
+    // example_1::demo();
+    example_2::demo();
 }
