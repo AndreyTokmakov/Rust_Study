5c0965f733aed3f526a691a3b3ace88529805b50
Andrei.Tokmakov <andtokm@yandex.ru>
2025-10-11 21:48:19 +0400

    11_10_2025_05

--- ZeroMQ/src/clients.rs
+++ ZeroMQ/src/clients.rs
@@ -1,10 +1,31 @@
 
 mod clients
 {
+    use std::thread;
+    use std::thread::sleep;
+    use zmq::{Context, Socket};
+    use std::time::Duration;
 
+    pub fn req_client()
+    {
+        let context: Context = zmq::Context::new();
+        let socket: Socket =  context.socket(zmq::REQ).unwrap();
+        socket.connect("tcp://localhost:5556").unwrap();
+
+        for i in 0..3 {
+            let request = format!("Hello {}", i);
+            println!("Sending: {}", request);
+            socket.send(&request, 0).unwrap();
+
+            let reply = socket.recv_string(0).unwrap().unwrap();
+            println!("Received reply: {}", reply);
+
+            thread::sleep(Duration::from_millis(250));
+        }
+    }
 }
 
 pub fn test_all()
 {
-
+    clients::req_client();
 }
